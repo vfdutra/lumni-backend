b0f1612c9f738f3a05dd6c588c986ad50a5d933d
Luis Miguel <lmcorrea@inf.ufsm.br>
2022-10-06 13:52:47 -0300

    fixing all and adding cors

--- database/migrations/1664749087070_players.ts
+++ database/migrations/1664749087070_players.ts
@@ -6,11 +6,11 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE')
       table.integer('ranking')
       table.integer('score')
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
+      table.integer('user_id').unsigned().references('id').inTable('users')
     })
   }
 
