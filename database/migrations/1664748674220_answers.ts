f32d71654f5f383d0e4cef551e6d966f6110eeb8
Luis Miguel <lmcorrea@inf.ufsm.br>
2022-10-06 13:40:43 -0300

    Revert "Ajuste migração"
    This reverts commit a64d382bc881fc70cc334e873946c642337c3bb7.

--- database/migrations/1664748674220_answers.ts
+++ database/migrations/1664748674220_answers.ts
@@ -3,19 +3,19 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'answers'
 
-  public async up() {
+  public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
+      table.integer('player_id').unsigned().references('id').inTable('players').onDelete('CASCADE').onUpdate('CASCADE')
+      table.integer('problem_id').unsigned().references('id').inTable('problems').onDelete('CASCADE').onUpdate('CASCADE')
       table.string('answer', 255).notNullable()
       table.bigInteger('used_time').notNullable()
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
-      table.integer('player_id').unsigned().references('id').inTable('players')
-      table.integer('problem_id').unsigned().references('id').inTable('problems')
     })
   }
 
-  public async down() {
+  public async down () {
     this.schema.dropTable(this.tableName)
   }
 }
