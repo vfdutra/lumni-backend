9e3e21dcaec448e4acbc7bab5304dd57ef6f7414
Vinicius Dutra <vfdutra@gmail.com>
2022-10-13 16:28:48 -0300

    Ajustes de deploy

--- config/database.ts
+++ config/database.ts
@@ -101,6 +101,9 @@ const databaseConfig: DatabaseConfig = {
         user: Env.get('PG_USER', DATABASE_URL.username),
         password: Env.get('PG_PASSWORD', DATABASE_URL.password),
         database: Env.get('PG_DB_NAME', DATABASE_URL.pathname.substr(1)),
+        ssl: {
+          rejectUnauthorized: false
+        }
       },
       migrations: {
         naturalSort: true,
