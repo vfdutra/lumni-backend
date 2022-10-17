26ec6b08ea0af28b3bb987d66955f3c20e3746ae
Vinicius Dutra <vfdutra@gmail.com>
2022-10-17 14:43:16 -0300

    Teste deploy automático

--- start/routes.ts
+++ start/routes.ts
@@ -30,7 +30,7 @@ Route.get('/logout', 'SessionsController.delete').as('sessions.delete')
 
 Route.post('/users', 'UsersController.store').as('users.store')
 Route.get('/users', async () => {
-  return { status: '300' }
+  return { status: '400' }
 })
 Route.put('/users/:id', 'UsersController.update').as('users.update')
 
