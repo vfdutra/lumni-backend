448a25e40552f353c3508a26cd766a40a347ac0d
Matheus Garcia Escobar <mgescobar@inf.ufsm.br>
2022-10-25 19:56:02 -0300

    working in progress

--- app/Controllers/Http/ProblemsController.ts
+++ app/Controllers/Http/ProblemsController.ts
@@ -1,10 +1,27 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Option from 'App/Models/Option';
 import Problem from 'App/Models/Problem'
 
 export default class ProblemsController {
     public async store({ request, response }: HttpContextContract) {
         const problemsList = request.input("problems");
-        const problems = await Problem.createMany(problemsList)
+        //const problems = await Problem.createMany(problemsList)
+        const problems = problemsList.map(problem => ({
+            description: problem.description,
+            level: problem.level,
+            tips: problem.tips,
+            options: problem.options.map(option => ({
+                description: option.description,
+                problem_id: problem.id,
+                correct: option.correct
+            }))
+        }))
+        const problemss = await Problem.createMany(problems)
+        console.log(problems[0])
+
+
+
+
         return response.ok({ problems })
     }
     
