7a915c7cca19333b3da74068b7f05d8f40af37ae
Vinicius Dutra <vfdutra@gmail.com>
2022-12-05 15:20:47 -0300

    Criação de migration para tabela level

---
+++ database/migrations/1670259041112_levels.ts
@@ -0,0 +1,16 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'levels'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('description', 255).notNullable()
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
