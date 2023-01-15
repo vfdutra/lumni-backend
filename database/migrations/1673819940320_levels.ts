3644d3619c453d997e870004d04236886088f49b
Matheus Garcia Escobar <mgescobar@inf.ufsm.br>
2023-01-15 20:53:21 -0300

    Update 1673819940320_levels.ts

--- database/migrations/1673819940320_levels.ts
+++ database/migrations/1673819940320_levels.ts
@@ -1,16 +1,16 @@
-import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+// import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
-export default class extends BaseSchema {
-  protected tableName = 'levels'
+// export default class extends BaseSchema {
+//   protected tableName = 'levels'
 
-  public async up () {
-    this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-      table.string('description', 255).notNullable()
-    })
-  }
+//   public async up () {
+//     this.schema.createTable(this.tableName, (table) => {
+//       table.increments('id')
+//       table.string('description', 255).notNullable()
+//     })
+//   }
 
-  public async down () {
-    this.schema.dropTable(this.tableName)
-  }
-}
+//   public async down () {
+//     this.schema.dropTable(this.tableName)
+//   }
+// }
