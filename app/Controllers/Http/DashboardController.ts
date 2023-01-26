2cea1df8710407d65ee6aa8a00f4d60933f4061d
Vinicius Dutra <vfdutra@gmail.com>
2023-01-26 12:55:12 -0300

    Cria rota para retorno de acertos, erros e média por usuário e adiciona total de perguntas em rota previamente criada

--- app/Controllers/Http/DashboardController.ts
+++ app/Controllers/Http/DashboardController.ts
@@ -2,17 +2,16 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Problem from 'App/Models/Problem'
 import Option from 'App/Models/Option'
 import Answer from 'App/Models/Answer'  
-
-
+import Player from 'App/Models/Player'
 export default class DashboardController {
   public async numberOfQuestionsByThemes({ response }: HttpContextContract) {
-    const problems = await Problem.query()
+    const problems = await Problem.all()
     const themes = await Problem.query().select('theme').distinct()
     const numberOfQuestionsByThemes = themes.map((theme) => {
       const numberOfQuestions = problems.filter(problem => problem.theme === theme.theme).length  
       return { theme: theme.theme, questions: numberOfQuestions }
     })
-    return response.ok({ numberOfQuestionsByThemes })
+    return response.ok({ numberOfQuestionsByThemes, total: problems.length })
   }
 
   public async answersStatsByThemes ({ response }: HttpContextContract) {
@@ -27,13 +26,37 @@ export default class DashboardController {
       let countCorrect = 0;
       problems.filter(problem => problem.theme === theme.theme).map((problem) => {
         const correctOption = options.filter(option => option.problem_id === problem.id && option.correct === 1)[0]
-        const correctAnswers = answers.filter(answer => answer.id_answer === correctOption.id).length
+        const correctAnswers = answers.filter(answer => answer.option_id === correctOption.id).length
         countCorrect += correctAnswers
       })
       const countWrong = numberOfAnswersByTheme-countCorrect;
       return { theme: theme.theme, numberOfQuestions: problemsByTheme.length, numberOfAnswersByTheme: numberOfAnswersByTheme, correctAnswers: countCorrect, wrongAnswers: countWrong }
     })
     return response.ok({ answerStatsByTheme })
+  }
+
+  public async answersByPlayer ({ response }: HttpContextContract) {
+    const player = await Player.all()    
+    const playerData = await Promise.all(
+      player.map(async (player) => {
+        const answers = await Answer.query().where('player_id', player.id)
+        const options = await Promise.all(
+          answers.map(async (answer) => {
+            return await Option.query().where('id', answer.option_id)
+          })
+        )
+        const correctAnswers = options
+          .flat()
+          .filter((option) => option.correct === 1).length
+        const wrongAnswers = options
+          .flat()
+          .filter((option) => option.correct === 0).length
+        const average = (correctAnswers / (correctAnswers + wrongAnswers)) * 100
+        
+        return {...player.$attributes, correctAnswers, wrongAnswers, average }
+      })
+    )
+    return response.ok(playerData)
   }
 
 }
